a6470ab8ee1a67c7416b2f3b2ff2758eaf013609
Scott <45034684+eeegs@users.noreply.github.com>
2024-10-12 12:58:46 +1100

    Added some comments

--- ModelsEtc/ViewModel.cs
+++ ModelsEtc/ViewModel.cs
@@ -6,11 +6,13 @@ namespace ModelsEtc
 
     public partial class ViewModel : ObservableObject
     {
+        // there are a fixed number of seats
         [ObservableProperty]
         ObservableCollection<Seat> seats = [new(), new(), new(), new(), new()];
 
         public ViewModel()
         {
+            // occupy two seats with players
             Seats[0].Player = new Player { Name = "Alice" };
             Seats[2].Player = new Player { Name = "Bob" };
         }
